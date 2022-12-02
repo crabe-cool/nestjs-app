b937dd73ff0c645bcfdd15e9256005dd0527cfeb
Pierre <pierre.viara@outlook.com>
2022-12-02 04:26:08 +0100

    tech(app): use helmet or cors depending on app environment

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 import { IConfigurationService } from './infrastructure/configuration/configuration-service.interface';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -9,6 +10,16 @@ async function bootstrap() {
     const configurationService = app.get<IConfigurationService>(
         'ConfigurationService',
     );
+    
+    if (configurationService.application.isEnvDev) {
+        app.enableCors();
+    } else {
+        app.use(
+            helmet({
+                contentSecurityPolicy: false,
+            }),
+        );
+    }
 
     app.useGlobalPipes(new ValidationPipe());
 
